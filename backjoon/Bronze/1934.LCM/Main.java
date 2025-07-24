077d21f4ee10a9b5713b23920cef56cdabc2ae5d
KIM JIHYUN <85729769+JIHYUNY2@users.noreply.github.com>
2025-07-25 00:58:45 +0900

    Update Main.java

--- backjoon/Bronze/1934.LCM/Main.java
+++ backjoon/Bronze/1934.LCM/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;  // 표준 입력을 받기 위해 Scanner 클래스 임포트
 
-public class Main {  // 백준 제출용 클래스명은 반드시 Main
+public class Main {  
     /**
      * 최대공약수(GCD)를 구하는 함수
      * 유클리드 호제법 사용: GCD(a, b) = GCD(b, a % b)
@@ -23,14 +23,14 @@ public class Main {  // 백준 제출용 클래스명은 반드시 Main
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  // Scanner로 입력 받기
+        Scanner sc = new Scanner(System.in);  
 
         int T = sc.nextInt();  // 테스트 케이스 개수 입력 받기
 
         // 각 테스트 케이스마다 A와 B를 입력받아 LCM을 계산
         for (int i = 0; i < T; i++) {
-            int A = sc.nextInt();  // 첫 번째 자연수 A
-            int B = sc.nextInt();  // 두 번째 자연수 B
+            int A = sc.nextInt();  
+            int B = sc.nextInt(); 
             System.out.println(lcm(A, B));  // 최소공배수 출력
         }
     }
