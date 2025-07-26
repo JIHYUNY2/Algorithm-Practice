96cf3754fb1200c17f0aad3619d3add49cd33958
KIM JIHYUN <85729769+JIHYUNY2@users.noreply.github.com>
2025-07-26 23:33:54 +0900

    Update Main.java

--- backjoon/Bronze/2739.GUGUDAN/Main.java
+++ backjoon/Bronze/2739.GUGUDAN/Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // 입력을 받기 위한 Scanner
+        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); // 구구단 단 수 입력 (1 ≤ N ≤ 9)
+        int N = sc.nextInt();  // 구구단 단 수 입력 받기 (1 ~ 9)
 
+        // 1부터 9까지 곱셈 결과 출력
         for (int i = 1; i <= 9; i++) {
-            System.out.println(N + " * " + i + " = " + (N * i));
+            System.out.println(N + " * " + i + " = " + (N * i));  // 출력 형식에 맞게 출력
         }
     }
 }
